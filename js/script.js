617a042729978a9560b5bec391652ef84061d666
Nahuel Genchi <nagen01@gmail.com>
2022-05-04 20:31:01 -0300

    Showing/hiding popup form

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 const addBox = document.querySelector(".add-box");
 const popupBox = document.querySelector(".popup-box");
+const closeIcon = popupBox.querySelector("header i");
+
+addBox.addEventListener("click", function(){
+    popupBox.classList.add("show");
+});
+closeIcon.addEventListener("click", function(){
+    popupBox.classList.remove("show");
+});
