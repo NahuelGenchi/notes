388155f1f7588922b2c169e80a557e2282d2c0a8
Nahuel Genchi <nagen01@gmail.com>
2022-05-05 23:32:04 -0300

    Showing all notes from localStorage

--- js/script.js
+++ js/script.js
@@ -15,8 +15,33 @@ addBox.addEventListener("click", function(){
     popupBox.classList.add("show");
 });
 closeIcon.addEventListener("click", function(){
+    titleTag.value = "";
+    descTag.value = "";
     popupBox.classList.remove("show");
 });
+function showNotes() {
+    document.querySelectorAll(".note").forEach(note => note.remove());
+    notes.forEach((note) => {
+        let liTag = `<li class="note">
+                        <div class="details">
+                            <p>${note.title}</p>
+                            <span>${note.description}</span>
+                        </div>
+                        <div class="bottom-content">
+                            <span>${note.date}</span>
+                            <div class="settings">
+                                <i class="uil uil-ellipsis-h"></i>
+                                <ul class="menu">
+                                    <li><i class="uil uil-pen"></i>Edit</li>
+                                    <li><i class="uil uil-trash"></i>Delete</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </li>`;
+        addBox.insertAdjacentHTML("afterend", liTag);
+    });
+};
+showNotes();
 addBtn.addEventListener("click", e => {
     e.preventDefault();
     let noteTitle = titleTag.value,
@@ -36,5 +61,6 @@ addBtn.addEventListener("click", e => {
         // saving notes to localStorage
         localStorage.setItem("notes", JSON.stringify(notes));
         closeIcon.click();
+        showNotes();
     }
 });
